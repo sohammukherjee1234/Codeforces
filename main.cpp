8bea813a2381b537f1c7f3fd5078167eb05ba9fc
soham mukherjee <rockstarsam1234@gmail.com>
2018-10-06 22:43:27 +0530

    Add files via upload
    hackoctoberfest

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define loopb(i,a,b) for(int i=a;i>=b;i--)
+#define loopm(i,a,b,step) for(int i=a;i<b;i+=step)
+#define loopbm(i,a,b,step) for(int i=a;i>=b;i-=step)
+#define pb(a) push_back(a)
+#define mp(a,b) make_pair(a,b)
+#define init(arr,val) memset(arr,val,sizeof(arr))
+#define INF 1000000007
+#define MOD 1000000007
+#define BINF 1000000000000000001
+#define int long long int
+#define double long double
+
+using namespace std;
+
+#undef int
+int main()
+{
+#define int long long int
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    return 0;
+}
